0c815a77db965606c6f6cce3ce4b04a1a0069da7
Administrator <rsingh241020@gmail.com>
2026-02-06 11:08:45 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/pooling/controller/BookingController.java
+++ src/main/java/com/example/pooling/controller/BookingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bookings")
+@RequestMapping("/api/bookings")
 public class BookingController {
 
     private final BookingService bookingService;
